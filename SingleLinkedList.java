1cd1aee3b380f407a68bc1d8e931c43d7253cef3
Yu Knag <ykang9533@naver.com>
2021-09-11 10:48:04 +0900

    # basic

---
+++ SingleLinkedList.java
@@ -0,0 +1,107 @@
+package com.company.basic;
+
+public class SingleLinkedList<T> {
+    public Node<T> head = null;
+
+    public class Node<T> {
+        T data;
+        Node<T> next = null;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    public void addNode(T data) {
+        if (head == null) {
+            head = new Node<T>(data);
+        } else {
+            Node<T> node = this.head;
+            while (node.next != null) {
+                node = node.next;
+            }
+            node.next = new Node<T>(data);
+        }
+    }
+
+    public void printAll() {
+        if (head != null) {
+            Node<T> node = this.head;
+            System.out.println(node.data);
+            while (node.next != null) {
+                node = node.next;
+                System.out.println(node.data);
+            }
+        }
+    }
+
+    public Node<T> search(T data) {
+        if (this.head == null) {
+            return null;
+        } else {
+            Node<T> node = this.head;
+            while(node != null) {
+                if (node.data == data) {
+                    return node;
+                } else {
+                    node = node.next;
+                }
+            }
+            return null;
+        }
+    }
+
+    public void addNodeInside(T data, T isData) {
+        Node<T> searchedNode = this.search(isData);
+
+        if (searchedNode == null) {
+            this.addNode(data);
+        } else {
+            Node<T> nextNode = searchedNode.next;
+            searchedNode.next = new Node<T>(data);
+            searchedNode.next.next = nextNode;
+        }
+    }
+
+    public boolean deleteNode(T isData) {
+        // 삭제할 데이터가 없을때
+        if(this.head == null){
+            return false;
+        }
+        //삭제할 데이터를 찾으면
+        else {
+            Node<T> node = this.head;
+            if (node.data == isData) {
+                this.head = this.head.next;
+                return true;
+            } else {
+                while (node.next != null) {
+                    if (node.next.data == isData) {
+                        node.next = node.next.next;
+                        return true;
+                    }
+                    node = node.next;
+                }
+                return false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SingleLinkedList<Integer> myList = new SingleLinkedList<Integer>();
+        //링크드리스트 생성하고 데이터 넣기
+        myList.addNode(1);
+        myList.addNode(2);
+        myList.addNode(3);
+        myList.addNode(4);
+        //1 데이터 뒤에 5 넣어보기
+        myList.addNodeInside(5,1);
+        myList.printAll();
+        System.out.println("------------------------------------------");
+
+        System.out.println(myList.deleteNode(3));
+        System.out.println( myList.deleteNode(3));
+
+        myList.printAll();
+    }
+}
