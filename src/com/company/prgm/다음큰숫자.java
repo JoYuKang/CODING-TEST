793398778d03bc5d7b7758b42c926277a7a4ea11
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2021-09-29 14:30:30 +0900

    # 더움 큰 숫자

--- src/com/company/prgm/다음큰숫자.java
+++ src/com/company/prgm/다음큰숫자.java
@@ -3,15 +3,40 @@ package com.company.prgm;
 public class 다음큰숫자 {
 
     public static int solution(int n) {
-        int cnt = Integer.bitCount(n);
-        while (true){
+        int answer = 0;
+        int cnt = bitCount(n);
+
+       n++;
+        while (cnt != bitCount(n)) {
             n++;
-            if(cnt == Integer.bitCount(n)){
-                return n;
-            }
         }
+
+        answer = n;
+
+        return answer;
+
+        // 기존 코드
+//        while (true) {
+//            n++;
+//            if (cnt == bitCount(n)) {
+//                return n;
+//            }
+//        }
+
     }
 
+    //bitCount 직접구현
+    public static int bitCount(int num) {
+        int cnt = 0;
+        String str = Integer.toBinaryString(num);
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '1') {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
 
 
     public static void main(String[] args) {
