71988c9f14407ccec0b412e6fc18fc4746314e9b
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2021-10-03 22:51:14 +0900

    # 영어 끝말잇기

---
+++ src/com/company/prgm/영어끝말잇기.java
@@ -0,0 +1,36 @@
+package com.company.prgm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class 영어끝말잇기 {
+    public static int[] solution(int n, String[] words) {
+        int[] answer = {0, 0};
+
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add(words[0]);
+        for (int i = 1; i < words.length; i++) {
+            String check = arr.get(arr.size() - 1);
+
+            if (arr.contains(words[i]) || check.charAt(check.length() - 1) != words[i].charAt(0)) {
+                answer[0] = arr.size() % n + 1;
+                if ((arr.size() + 1) % n != 0) {
+                    answer[1] = (arr.size() + 1) / n + 1;
+                } else {
+                    answer[1] = (arr.size() + 1) / n;
+                }
+                break;
+            }
+            arr.add(words[i]);
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        String[] words = {"hello", "one", "even", "never", "now", "world", "draw"};
+        System.out.println(Arrays.toString(solution(n, words)));
+    }
+}
