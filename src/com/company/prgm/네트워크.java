5d1c38c362122149ebf9c7f23cc038361c027eed
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2021-10-18 15:10:44 +0900

    # 네트워크 리팩토링

--- src/com/company/prgm/네트워크.java
+++ src/com/company/prgm/네트워크.java
@@ -2,27 +2,26 @@ package com.company.prgm;
 
 public class 네트워크 {
 
-    static boolean visit[];
 
     public static int solution(int n, int[][] computers) {
         int answer = 0;
-
+        boolean visit[];
         visit = new boolean[computers[0].length];
 
         for (int i = 0; i < computers.length; i++) {
             if (!visit[i]) {
-                dfs(i, computers);
+                dfs(i, computers, visit);
                 answer++;
             }
         }
         return answer;
     }
 
-    private static void dfs(int n, int[][] computers) {
+    private static void dfs(int n, int[][] computers,boolean[] visit) {
         visit[n] = true;
         for (int i = 0; i < computers[0].length; i++) {
             if (!visit[i] && computers[n][i] == 1) {
-                dfs(i, computers);
+                dfs(i, computers,visit);
 
             }
         }
